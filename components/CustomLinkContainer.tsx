4f6b70bf8c7ab6ab6846eb4bbbe40cdb1424ff2b
Aditya Gupta <adityagupta765469@gmail.com>
2023-05-13 16:42:41 +0530

    modified number of grid

--- components/CustomLinkContainer.tsx
+++ components/CustomLinkContainer.tsx
@@ -4,7 +4,7 @@ import CustomLinkInput from "./CustomLinkInput"
 
 const CustomLinkContainer = () => {
   return (
-      <div className='fixed bg-black h-screen col-end-13 col-span-3 p-4 right-0'>
+      <div className='fixed bg-black h-screen col-end-13 col-span-2 p-4 right-0'>
       <WalletDetails />
       <CustomLinkInput />
       </div>
