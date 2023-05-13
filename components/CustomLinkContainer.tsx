fb9bea4fa24b8182d56276a9f8515f8a7fa2e454
Aditya Gupta <adityagupta765469@gmail.com>
2023-05-14 00:32:29 +0530

    updated grid

--- components/CustomLinkContainer.tsx
+++ components/CustomLinkContainer.tsx
@@ -4,7 +4,7 @@ import CustomLinkInput from "./CustomLinkInput"
 
 const CustomLinkContainer = () => {
   return (
-      <div className='fixed bg-black h-screen col-end-8 col-span-1 p-4 right-0 w-[15%]'>
+      <div className='fixed bg-black h-screen col-end-8 col-span-2 p-4 right-0 w-[28%]'>
       <WalletDetails />
       <CustomLinkInput />
       </div>
